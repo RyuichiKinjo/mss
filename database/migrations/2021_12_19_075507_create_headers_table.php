7395b74812d0763fd32f41a59499680f6c2b3b97
Ryuichi Kinjo <pgrypz@gmail.com>
2023-05-01 21:59:17 +0900

    notnull制約解除

--- database/migrations/2021_12_19_075507_create_headers_table.php
+++ database/migrations/2021_12_19_075507_create_headers_table.php
@@ -17,10 +17,10 @@ class CreateHeadersTable extends Migration
 
             // column
             $table->integer('user_id')->comment('user_id');
-            $table->string('name')->comment('氏名');
-            $table->string('initial')->comment('イニシャル');
+            $table->string('name')->nullable()->comment('氏名');
+            $table->string('initial')->nullable()->comment('イニシャル');
             $table->boolean('disp_name')->nullable()->comment('氏名表示');
-            $table->string('sex')->comment('性別');
+            $table->string('sex')->nullable()->comment('性別');
             $table->date('birthday')->nullable()->comment('生年月日');
             $table->string('station')->nullable()->comment('最寄り駅');
             $table->string('cert')->nullable()->comment('資格');
